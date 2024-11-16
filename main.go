12dddfb8031344cdcd5114f19583eeb5d60c1426
nem0z <maxime.bernard.dev@gmail.com>
2024-11-16 14:55:21 +0100

    feat: add a function to verify the service output

--- main.go
+++ main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"sort"
 	"time"
 
 	topKService "github.com/nem0z/amazon-loop/topK"
 )
+
+func verify(top []int, freqs map[int]int) bool {
+	sortedFreq := make([]int, len(freqs))
+	i := 0
+	for _, freq := range freqs {
+		sortedFreq[i] = freq
+		i++
+	}
+
+	sort.Ints(sortedFreq)
+
+	for _, id := range top {
+		if freqs[id] < sortedFreq[len(sortedFreq)-len(top)] {
+			return false
+		}
+	}
+
+	return true
+}
 
 func main() {
 
@@ -24,6 +45,8 @@ func main() {
 
 			ids, freqs := topK.Collect()
 			fmt.Println(ids)
+			if !verify(ids, freqs) {
+				log.Fatal("Invalid result :", freqs)
 			}
 		}
 	}()
